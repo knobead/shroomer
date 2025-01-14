3201e95d6cd590af90f38fc3a143bb2015d91c9b
thomas <tgondardmary@gmail.com>
2025-01-15 00:21:11 +0100

    fix: fix test

--- tests/Generator/Tree/TreeGeneratorTest.php
+++ tests/Generator/Tree/TreeGeneratorTest.php
@@ -6,6 +6,7 @@ namespace App\Tests\Generator\Tree;
 use App\Entity\Tree;
 use App\Generator\Handler\GenerateTreeHandler;
 use App\Generator\Message\GenerateTreeMessage;
+use App\Repository\MyceliumRepository;
 use App\Tests\FixtureLoaderCapableTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -23,13 +24,14 @@ class TreeGeneratorTest extends WebTestCase
     {
         /** @var Tree $tree */
         $tree = $this->fixturesRepository->getReference(TreeGeneratorFixtures::FIRST_TREE_REFERENCE);
+        $myceliumRepository = $this->client->getContainer()->get(MyceliumRepository::class);
 
         $generator = self::getContainer()->get(GenerateTreeHandler::class);
         $generator->__invoke(new GenerateTreeMessage($tree->getId()));
 
-        
+        $myceliums = $myceliumRepository->findByTree($tree);
 
         self::assertSame(56, $tree->getAge());
-        self::assertCount(1, $tree->getMyceliums());
+        self::assertCount(1, $myceliums);
     }
 }
