730b2e885f49e09b669414c469658ed467676a02
thomas <tgondardmary@gmail.com>
2025-01-22 20:26:37 +0100

    feat: init fixtures

--- src/DataFixtures/ZoneFixtures.php
+++ src/DataFixtures/ZoneFixtures.php
@@ -2,15 +2,38 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Zone;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class ZoneFixtures extends Fixture
 {
+    public const string ZONE_ONE_REFERENCE = 'zone_one';
+    public const string ZONE_TWO_REFERENCE = 'zone_two';
+    public const string ZONE_THREE_REFERENCE = 'zone_three';
+    public const string ZONE_FOUR_REFERENCE = 'zone_four';
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $zoneOne = new Zone();
+        $zoneOne->setName('zone one');
+        $manager->persist($zoneOne);
+        $this->addReference(self::ZONE_ONE_REFERENCE, $zoneOne);
+
+        $zoneTwo = new Zone();
+        $zoneTwo->setName('zone two');
+        $manager->persist($zoneTwo);
+        $this->addReference(self::ZONE_TWO_REFERENCE, $zoneTwo);
+
+        $zoneThree = new Zone();
+        $zoneThree->setName('zone three');
+        $manager->persist($zoneThree);
+        $this->addReference(self::ZONE_THREE_REFERENCE, $zoneThree);
+
+        $zoneFour = new Zone();
+        $zoneFour->setName('zone four');
+        $manager->persist($zoneFour);
+        $this->addReference(self::ZONE_FOUR_REFERENCE, $zoneFour);
 
         $manager->flush();
     }
